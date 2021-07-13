a4a35bf39aa9dcf96b120cf7c5346742a1ddb2e7
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-07-13 13:49:10 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -4,35 +4,35 @@ let z = 0;
 
 switch (prompt("Elige tu opción: \n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir.")) {
   case "1": {
-    x = Number(prompt("Escribe el primer sumando"))
-    z = Number(prompt("Ahora escribe el segundo sumando"))
+    x = Number(prompt("Escribe el primer sumando"));
+    z = Number(prompt("Ahora escribe el segundo sumando"));
     let resultadoSuma = x + z;
-    alert("Tú resultado es " + resultadoSuma)
+    alert("Tú resultado es " + resultadoSuma);
     break;
   }
   case "2": {
-    x = Number(prompt("Escribe el valor del minuendo"))
-    z = Number(prompt("Ahora escribe el valor del sustraendo"))
+    x = Number(prompt("Escribe el valor del minuendo"));
+    z = Number(prompt("Ahora escribe el valor del sustraendo"));
     let resultadoResta = x - z;
-    alert("Tú resultado es " + resultadoResta)
+    alert("Tú resultado es " + resultadoResta);
     break;
   }
   case "3": {
-    x = Number(prompt("Especifica tu multiplicando"))
-    z = Number(prompt("Ahora especifica tu multilpicador"))
+    x = Number(prompt("Especifica tu multiplicando"));
+    z = Number(prompt("Ahora especifica tu multilpicador"));
     let resultadoMultiplicar = x * z;
-    alert("Tú resultado es " + resultadoMultiplicar)
+    alert("Tú resultado es " + resultadoMultiplicar);
     break;
   }
   case "4": {
-    x = Number(prompt("Especifica tu divisor"))
-    z = Number(prompt("Ahora especifica tu dividendo"))
+    x = Number(prompt("Especifica tu divisor"));
+    z = Number(prompt("Ahora especifica tu dividendo"));
     let resultadoDivision = x / z;
-    alert("Tú resultado es " + resultadoDivision)
+    alert("Tú resultado es " + resultadoDivision);
     break;
   }
   default: {
-    alert("No has seleccionado una opción válida.")
+    alert("No has seleccionado una opción válida.");
   }
  }
 }
