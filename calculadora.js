7a93487b86599a5f358ba6d956ce85e9c0a08262
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-07-13 13:17:08 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -4,29 +4,29 @@ let z = 0;
 
 switch (prompt("Elige tu opción: \n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir.")) {
   case "1": {
-    x = Number(prompt("Primer valor a sumar"))
-    z = Number(prompt("Segundo valor a sumar"))
+    x = Number(prompt("Escribe el primer sumando"))
+    z = Number(prompt("Ahora escribe el segundo sumando"))
     let resultadoSuma = x + z;
     alert("Tú resultado es " + resultadoSuma)
     break;
   }
   case "2": {
-    x = Number(prompt("Primer valor a restar"))
-    z = Number(prompt("Segundo valor a restar"))
+    x = Number(prompt("Escribe el valor del minuendo"))
+    z = Number(prompt("Ahora escribe el valor del sustraendo"))
     let resultadoResta = x - z;
     alert("Tú resultado es " + resultadoResta)
     break;
   }
   case "3": {
-    x = Number(prompt("Primer valor a multiplicar"))
-    z = Number(prompt("Segundo valor a multiplicar"))
+    x = Number(prompt("Especifica tu multiplicando"))
+    z = Number(prompt("Ahora especifica tu multilpicador"))
     let resultadoMultiplicar = x * z;
     alert("Tú resultado es " + resultadoMultiplicar)
     break;
   }
   case "4": {
-    x = Number(prompt("Primer valor a dividir"))
-    z = Number(prompt("Segundo valor a dividir"))
+    x = Number(prompt("Especifica tu divisor"))
+    z = Number(prompt("Ahora especifica tu dividendo"))
     let resultadoDivision = x / z;
     alert("Tú resultado es " + resultadoDivision)
     break;
